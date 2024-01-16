461293ec62cc16414dd42d39ab6e887b3d44ab0d
HugoDF <HugoDF@users.noreply.github.com>
2024-01-16 08:26:52 +0000

    add 06.02 direct fetch example, fix carets in deps

---
+++ src/06.02-native-fetch-intercept.test.js
@@ -0,0 +1,34 @@
+import { test } from "node:test";
+import assert from "node:assert/strict";
+import { MockAgent, setGlobalDispatcher } from "undici";
+
+test("native fetch interception via undici", async () => {
+	const mockAgent = new MockAgent();
+	setGlobalDispatcher(mockAgent);
+	mockAgent.disableNetConnect();
+
+	mockAgent
+		.get("https://api.github.com")
+		.intercept({ path: "/users/HugoDF" })
+		.reply(200, {
+			blog: "https://codewithhugo.com",
+			location: "London",
+			bio: "Developer, JavaScript",
+			public_repos: 39,
+		});
+
+	const data = await fetch("https://api.github.com/users/HugoDF").then((res) =>
+		res.json(),
+	);
+
+	assert.deepEqual(data, {
+		blog: "https://codewithhugo.com",
+		location: "London",
+		bio: "Developer, JavaScript",
+		public_repos: 39,
+	});
+
+	mockAgent.assertNoPendingInterceptors();
+
+	assert.deepEqual(mockAgent.pendingInterceptors(), []);
+});
