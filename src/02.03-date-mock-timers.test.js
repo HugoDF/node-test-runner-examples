72e18a050994440164197666d4c4f7c968bc83a0
HugoDF <HugoDF@users.noreply.github.com>
2024-01-21 10:51:04 +0000

    demonstrate mock.timers.tick and mock.timers.setTime

--- src/02.03-date-mock-timers.test.js
+++ src/02.03-date-mock-timers.test.js
@@ -3,8 +3,6 @@ import assert from "node:assert/strict";
 
 const isNode20OrLower = process.versions.node.split(".")[0] <= "20";
 
-const getCurrentDate = () => new Date();
-
 afterEach(() => {
 	mock.timers.reset();
 });
@@ -19,19 +17,38 @@ test(
 			now: new Date("2023-05-14T11:01:58.135Z"),
 		});
 
-		assert.deepEqual(getCurrentDate(), new Date("2023-05-14T11:01:58.135Z"));
+		assert.deepEqual(new Date(), new Date("2023-05-14T11:01:58.135Z"));
 
 		t.mock.timers.reset();
 		assert(new Date().getFullYear() > 2023);
 	},
 );
 
-test("mock.timers can mock Date.now()", { skip: isNode20OrLower }, (t) => {
-	t.mock.timers.enable({
-		// @ts-ignore
-		apis: ["Date"],
-		now: new Date("2023-05-14T11:01:58.135Z"),
-	});
+test(
+	"mock.timers can mock Date.now() and be ticked forward",
+	{ skip: isNode20OrLower },
+	(t) => {
+		t.mock.timers.enable({
+			// @ts-ignore
+			apis: ["Date"],
+			now: new Date("2023-05-14T11:01:58.135Z"),
+		});
 
-	assert.deepEqual(Date.now(), 1684062118135);
-});
+		assert.deepEqual(
+			Date.now(),
+			new Date("2023-05-14T11:01:58.135Z").valueOf(),
+		);
+
+		// advance by 24h
+		t.mock.timers.tick(1000 * 60 * 60 * 24);
+
+		assert.deepEqual(
+			Date.now(),
+			new Date("2023-05-15T11:01:58.135Z").valueOf(),
+		);
+
+		// @ts-ignore
+		t.mock.timers.setTime(20000);
+		assert.deepEqual(Date.now(), 20000);
+	},
+);
